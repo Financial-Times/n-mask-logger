5902be8c079c875e2b2e7d0c4a9a47b1cf7e9ad8
Laura Carvajal <laura.carvajal@gmail.com>
2016-05-16 15:01:16 +0100

    tests

--- src/main.js
+++ src/main.js
@@ -5,35 +5,73 @@ const MASK_SEQUENCE = '*****';
 class SafeLogger {
 
 	constructor (sensitiveFields=[]) {
-		this.safeLogger = logger;
 		this.sensitiveFields = sensitiveFields;
 	}
 
 	info (message) {
+		return this.log('info', message);
+	}
 
-		let maskedMessage = message;
+	warn (message) {
+		return this.log('warn', message);
+	}
+
+	error (message) {
+		return this.log('error', message);
+	}
+
+	log (type, message) {
+		let maskedMessage;
 
 		if (typeof message === 'object') {
-
-			let mask = {};
-
-			for (const field of this.sensitiveFields) {
-
-				if (message[field]) {
-					mask[field] = MASK_SEQUENCE;
-				}
-			}
-			maskedMessage = Object.assign({}, message, mask);
+			maskedMessage = {};
+			maskedMessage = this.maskMessage(message);
+			logger.info(JSON.stringify(maskedMessage));
+			return JSON.stringify(maskedMessage);
 		}
 		else {
+			maskedMessage = message;
 			for (const field of this.sensitiveFields) {
 				if (message.indexOf(field) !== -1) {
 					maskedMessage = MASK_SEQUENCE;
 					break;
 				}
 			}
+			logger[type](maskedMessage);
+			return maskedMessage;
 		}
-		logger.info('maskedMessage', maskedMessage);
+	}
+
+	maskMessage (message) {
+		let clonedMessage = JSON.parse(JSON.stringify(message));
+		let maskedField;
+
+		for (const field in message) {
+
+			if (message.hasOwnProperty(field)){
+
+				if (typeof message[field] === 'object') {
+					maskedField = this.maskMessage(message[field]);
+					clonedMessage[field] = maskedField;
+				}
+				else {
+					if (this.sensitiveFields.indexOf(field) !== -1) {
+						// Object KEY is a sentisitve field, mask the VALUE
+						clonedMessage[field] = MASK_SEQUENCE;
+					}
+					else {
+						// Does the VALUE of a safe KEY contain a sensitive word in itself?
+						for (const sensitiveField of this.sensitiveFields) {
+							if (message[field].indexOf(sensitiveField) !== -1) {
+								clonedMessage[field] = MASK_SEQUENCE;
+								break;
+							}
+						}
+					}
+				}
+			}
+		}
+		return clonedMessage;
 	}
 }
 export default SafeLogger;
--- test/logger.test.js
+++ test/logger.test.js
@@ -1,5 +1,105 @@
+// import sinon from 'sinon';
+import chai from 'chai';
+const expect = chai.expect;
+import chaiString from 'chai-string';
+chai.should();
+chai.use(chaiString);
+import SafeLogger from '../src/main';
+
 describe('Logger', () => {
 
 	describe('Logging', () => {
+
+		let logger;
+
+		beforeEach(() => {
+			logger = new SafeLogger(['password', 'email'])
+		});
+
+		it('STRING: Should log an empty object', () => {
+			const message = logger.info({});
+			message.should.equal('{}');
+		});
+
+		it('OBJECT: Should mask VALUE of sensitive KEY', () => {
+			const message = logger.info({
+				something: 'safe',
+				password: 'should not log this'
+			});
+			expect(message.indexOf('*****')).to.not.equal(-1);
+			expect(message.indexOf('should not log this')).to.equal(-1);
+		});
+
+		it('OBJECT: Should mask VALUE of sensitive KEY n-levels deep', () => {
+			const message = logger.info({
+				something: 'safe',
+				other: {
+					filler: 'filler',
+					object: {
+						anotherObject: {
+							password: 'should not log this'
+						},
+						dummy: 'dummy'
+					}
+				}
+			});
+			expect(message.indexOf('*****')).to.not.equal(-1);
+			expect(message.indexOf('should not log this')).to.equal(-1);
+		});
+
+		it('OBJECT: Should mask full VALUE if VALUE contains sensitive word', () => {
+			const message = logger.info({
+				something: 'safe',
+				other: {
+					filler: 'filler',
+					object: {
+						anotherObject: {
+							maskThis: 'has sensitive word password in it'
+						},
+						dummy: 'dummy'
+					}
+				}
+			});
+			expect(message.indexOf('*****')).to.not.equal(-1);
+			expect(message.indexOf('password')).to.equal(-1);
+		});
+
+
+		it('STRING: Should log empty string', () => {
+			const message = logger.info('');
+			message.should.equal('');
+		});
+
+		it('STRING: Should match n-logger output if safe', () => {
+			const message = logger.info('something safe');
+			message.should.equal('something safe');
+		});
+
+		it('STRING: Should mask all output if sensitive string detected', () => {
+			const message = logger.info('I have the sensitive word password in me. Mask me just in case!');
+			message.should.equal('*****');
+		});
+
+		it('STRING: Should mask full VALUE if KEY is sensitive word', () => {
+			const message = logger.info('Mostly safe but contains the word password. This should be masked.');
+			message.should.equal('*****');
+		});
+
+		it('STRING: Should warn', () => {
+			// TODO check the log type
+			const message = logger.warn('something');
+			message.should.equal('something');
+		});
+
+		it('STRING: Should error', () => {
+			// TODO check the log type
+			const message = logger.error('something');
+			message.should.equal('something');
+		});
+
+		it('EMPTY: Should log null', () => {
+			const message = logger.info(null);
+			JSON.stringify(message).should.equal('"null"');
+		});
 	})
 });
